c8bee8e00029b7f4e196a9aa421baa0a42122e87
mhmd-aashik <aashikdevelop@gmail.com>
2024-04-21 07:54:40 +0530

    done

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,4 +1,4 @@
-import EventForm from "@/components/shared/EventForm.1";
+import EventForm from "@/components/shared/EventForm";
 import { getEventById } from "@/lib/actions/event.actions";
 import { auth } from "@clerk/nextjs";
 
